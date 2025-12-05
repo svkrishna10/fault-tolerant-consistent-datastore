2a249b3f4abd7d44966b7ea077a14ee1447752c0
svkrishna10 <krishnavasu10@gmail.com>
2025-12-05 18:29:10 -0500

    Final changes

--- src/server/faulttolerance/MyDBFaultTolerantServerZK.java
+++ src/server/faulttolerance/MyDBFaultTolerantServerZK.java
@@ -7,8 +7,10 @@ import edu.umass.cs.nio.interfaces.NodeConfig;
 import edu.umass.cs.nio.nioutils.NIOHeader;
 import edu.umass.cs.nio.nioutils.NodeConfigUtils;
 import edu.umass.cs.utils.Util;
+import server.AVDBReplicatedServer;
 import server.ReplicatedServer;
 import server.MyDBSingleServer;
+
 import org.apache.zookeeper.*;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,13 +21,54 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watcher {
+/**
+ * This class should implement your replicated fault-tolerant database server if
+ * you wish to use Zookeeper or other custom consensus protocols to order client
+ * requests.
+ * <p>
+ * Refer to {@link server.ReplicatedServer} for a starting point for how to do
+ * server-server messaging or to {@link server.AVDBReplicatedServer} for a
+ * non-fault-tolerant replicated server.
+ * <p>
+ * You can assume that a single *fault-tolerant* Zookeeper server at the default
+ * host:port of localhost:2181 and you can use this service as you please in the
+ * implementation of this class.
+ * <p>
+ * Make sure that both a single instance of Cassandra and a single Zookeeper
+ * server are running on their default ports before testing.
+ * <p>
+ * You can not store in-memory information about request logs for more than
+ * {@link #MAX_LOG_SIZE} requests.
+ */
+public class MyDBFaultTolerantServerZK extends server.MyDBSingleServer implements Watcher {
 
+    /**
+     * Set this value to as small a value with which you can get tests to still
+     * pass. The lower it is, the faster your implementation is. Grader* will
+     * use this value provided it is no greater than its MAX_SLEEP limit.
+     */
     public static final int SLEEP = 100;
+
+    /**
+     * Set this to true if you want all tables drpped at the end of each run
+     * of tests by GraderFaultTolerance.
+     */
     public static final boolean DROP_TABLES_AFTER_TESTS = true;
+
+    /**
+     * Maximum permitted size of any collection that is used to maintain
+     * request-specific state, i.e., you can not maintain state for more than
+     * MAX_LOG_SIZE requests (in memory or on disk). This constraint exists to
+     * ensure that your logs don't grow unbounded, which forces
+     * checkpointing to
+     * be implemented.
+     */
     public static final int MAX_LOG_SIZE = 400;
+
     public static final int DEFAULT_PORT = 2181;
-    private static final int CHECKPOINT_INTERVAL = 200; // Checkpoint every 200 requests
+
+    // ZK and State variables
+    private static final int CHECKPOINT_INTERVAL = 200; 
 
     private final ZooKeeper zk;
     private final String electionPath = "/election";
@@ -44,14 +87,24 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
     private int lastExecutedSeq = -1;
     private int lastCheckpointedSeq = -1;
 
+    /**
+     * @param nodeConfig Server name/address configuration information read
+     * from
+     * conf/servers.properties.
+     * @param myID       The name of the keyspace to connect to, also the name
+     * of the server itself. You can not connect to any other
+     * keyspace if using Zookeeper.
+     * @param isaDB      The socket address of the backend datastore to which
+     * you need to establish a session.
+     * @throws IOException
+     */
     public MyDBFaultTolerantServerZK(NodeConfig<String> nodeConfig, String myID, InetSocketAddress isaDB) throws IOException {
-        super(new InetSocketAddress(nodeConfig.getNodeAddress(myID), 
-                nodeConfig.getNodePort(myID) - ReplicatedServer.SERVER_PORT_OFFSET), 
-              isaDB, 
-              myID);
-        
+        super(new InetSocketAddress(nodeConfig.getNodeAddress(myID),
+                nodeConfig.getNodePort(myID) - ReplicatedServer.SERVER_PORT_OFFSET), isaDB, myID);
+
         this.myID = myID;
 
+        // Initialize Messenger
         this.serverMessenger = new MessageNIOTransport<String, String>(myID, nodeConfig, 
             new AbstractBytePacketDemultiplexer() {
                 @Override
@@ -61,11 +114,14 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
                 }
             }, true);
 
+        // Initialize Cassandra Session
         this.cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
         this.session = cluster.connect(myID);
 
+        // Initialize ZooKeeper
         this.zk = new ZooKeeper("localhost:" + DEFAULT_PORT, 3000, this);
 
+        // TODO: Make sure to do any needed crash recovery here.
         initializeZK();
         restoreFromCheckpoint(); // CRITICAL: Restore before processing new requests
         runLeaderElection();
@@ -100,7 +156,7 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
         try {
             if (myZnodeId == null) {
                 myZnodeId = zk.create(electionPath + "/n_", myID.getBytes(), 
-                                      ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+                                    ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
             }
 
             List<String> children = zk.getChildren(electionPath, false);
@@ -133,7 +189,9 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
         }
     }
 
-    @Override
+    /**
+     * TODO: process bytes received from clients here.
+     */
     protected void handleMessageFromClient(byte[] bytes, NIOHeader header) {
         String request = new String(bytes);
         String sender = (header.sndr != null) ? header.sndr.toString() : "unknown";
@@ -157,6 +215,9 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
         }
     }
 
+    /**
+     * TODO: process bytes received from fellow servers here.
+     */
     protected void handleMessageFromServer(byte[] bytes, NIOHeader header) {
         handleMessageFromClient(bytes, header);
     }
@@ -256,7 +317,7 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
             String cpNode = checkpointPath + "/cp-" + String.format("%010d", lastExecutedSeq);
             try {
                 zk.create(cpNode, snapshot.toString().getBytes(), 
-                         ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                          ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
                 lastCheckpointedSeq = lastExecutedSeq;
                 
                 Logger.getLogger(this.getClass().getName()).log(Level.INFO, 
@@ -316,11 +377,11 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
                     if (keyVal.length > 1 && !keyVal[1].isEmpty()) {
                         // Has events
                         session.execute("INSERT INTO " + myID + ".grade (id, events) VALUES (" 
-                                       + id + ", [" + keyVal[1] + "])");
+                                        + id + ", [" + keyVal[1] + "])");
                     } else {
                         // Empty events list
                         session.execute("INSERT INTO " + myID + ".grade (id, events) VALUES (" 
-                                       + id + ", [])");
+                                        + id + ", [])");
                     }
                 }
             }
@@ -383,7 +444,9 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
         }
     }
 
-    @Override
+    /**
+     * TODO: Gracefully close any threads or messengers you created.
+     */
     public void close() {
         super.close();
         if (serverMessenger != null) serverMessenger.stop();
@@ -394,6 +457,18 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
         } catch (Exception e) {}
     }
 
+    public static enum CheckpointRecovery {
+        CHECKPOINT, RESTORE;
+    }
+
+    /**
+     * @param args args[0] must be server.properties file and args[1] must be
+     * myID. The server prefix in the properties file must be
+     * ReplicatedServer.SERVER_PREFIX. Optional args[2] if
+     * specified
+     * will be a socket address for the backend datastore.
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         new MyDBFaultTolerantServerZK(NodeConfigUtils.getNodeConfigFromFile
                 (args[0], ReplicatedServer.SERVER_PREFIX, ReplicatedServer
@@ -401,4 +476,5 @@ public class MyDBFaultTolerantServerZK extends MyDBSingleServer implements Watch
                 .getInetSocketAddressFromString(args[2]) : new
                 InetSocketAddress("localhost", 9042));
     }
+
 }
--- src/server/faulttolerance/MyDBReplicableAppGP.java
+++ src/server/faulttolerance/MyDBReplicableAppGP.java
@@ -1,16 +1,16 @@
 package server.faulttolerance;
 
 import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
+import com.datastax.driver.core.ColumnDefinitions;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
-import com.datastax.driver.core.ColumnDefinitions;
-
+import com.datastax.driver.core.Session;
 import edu.umass.cs.gigapaxos.interfaces.Replicable;
 import edu.umass.cs.gigapaxos.interfaces.Request;
 import edu.umass.cs.gigapaxos.paxospackets.RequestPacket;
 import edu.umass.cs.nio.interfaces.IntegerPacketType;
 import edu.umass.cs.reconfiguration.reconfigurationutils.RequestParseException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -21,215 +21,265 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.json.JSONObject;
-
 /**
+ * This class should implement your {@link Replicable} database app if you wish
+ * to use Gigapaxos.
+ * <p>
+ * Make sure that both a single instance of Cassandra is running at the default
+ * port on localhost before testing.
+ * <p>
+ * Tips:
+ * <p>
+ * 1) No server-server communication is permitted or necessary as you are using
+ * gigapaxos for all that.
+ * <p>
+ * 2) A {@link Replicable} must be agnostic to "myID" as it is a standalone
+ * replication-agnostic application that via its {@link Replicable} interface is
+ * being replicated by gigapaxos. However, in this assignment, we need myID as
+ * each replica uses a different keyspace (because we are pretending different
+ * replicas are like different keyspaces), so we use myID only for initiating
+ * the connection to the backend data store.
+ * <p>
+ * 3) This class is never instantiated via a main method. You can have a main
+ * method for your own testing purposes but it won't be invoked by any of
+ * Grader's tests.
+ */
+public class MyDBReplicableAppGP implements Replicable {
 
-* MyDBReplicableAppGP implements the {@link Replicable} interface to allow
-* Gigapaxos to manage replicated state for a Cassandra-backed database.
-*
-* This version has updated comments and improved readability while keeping
-* key variable names consistent.
-  */
-  public class MyDBReplicableAppGP implements Replicable {
+    /**
+     * Set this value to as small a value with which you can get tests to still
+     * pass. The lower it is, the faster your implementation is. Grader* will
+     * use this value provided it is no greater than its MAX_SLEEP limit.
+     * Faster
+     * is not necessarily better, so don't sweat speed. Focus on safety.
+     */
+    public static final int SLEEP = 1000;
 
-  protected Cluster cluster;
-  protected Session session;
+    // User-defined fields
+    protected Cluster cluster;
+    protected Session session;
+    protected static final Logger log = Logger.getLogger(MyDBReplicableAppGP.class.getName());
+    protected String myID;    // Keyspace name
+    protected String table = "grade";   // Single table assumed for this assignment
 
-  protected static final Logger log = Logger.getLogger(MyDBReplicableAppGP.class.getName());
-
-  protected String myID;    // Keyspace name
-  protected String table;   // Single table assumed for this assignment
-
-  public static final int SLEEP = 1000; // Default sleep interval for tests
-
-  /**
-
-  * Constructor for the Gigapaxos replicable application.
-  *
-  * @param args args[0] should contain the Cassandra keyspace name
-  * @throws IOException
-    */
+    /**
+     * All Gigapaxos apps must either support a no-args constructor or a
+     * constructor taking a String[] as the only argument. Gigapaxos relies on
+     * adherence to this policy in order to be able to reflectively construct
+     * customer application instances.
+     *
+     * @param args Singleton array whose args[0] specifies the keyspace in the
+     * backend data store to which this server must connect.
+     * Optional args[1] and args[2]
+     * @throws IOException
+     */
     public MyDBReplicableAppGP(String[] args) throws IOException {
-    if (args == null || args.length == 0) {
-    throw new IOException("No keyspace provided in args[0]");
+        // TODO: setup connection to the data store and keyspace
+        if (args == null || args.length == 0) {
+            throw new IOException("No keyspace provided in args[0]");
+        }
+
+        // Connect to Cassandra cluster and keyspace
+        // Note: Hardcoded to localhost as per assignment requirements
+        cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+        session = cluster.connect(args[0]);
+
+        myID = args[0];
+        
+        // Log initialization info
+        for (int i = 0; i < args.length; i++) {
+            log.log(Level.INFO, "Startup arg {0}: {1}", new Object[]{i, args[i]});
+        }
+        log.log(Level.INFO, "Gigapaxos replicable initialized with keyspace: {0}", new Object[]{myID});
+        System.out.println("Replicable Giga Paxos started with keyspace " + myID);
     }
 
-    // Connect to Cassandra cluster and keyspace
-    cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-    session = cluster.connect(args[0]);
-
-    myID = args[0];
-    table = "grade"; // Default table used in this assignment
-
-    // Log initialization info
-    for (int i = 0; i < args.length; i++) {
-    log.log(Level.INFO, "Startup arg {0}: {1}", new Object[]{i, args[i]});
-    }
-    log.log(Level.INFO, "Gigapaxos replicable initialized with keyspace: {0}", new Object[]{myID});
-    System.out.println("Replicable Giga Paxos started with keyspace " + myID);
-    }
-
-  /**
-
-  * Executes a given request atomically.
-  *
-  * @param request The incoming request
-  * @param skipClientResponse If true, no response is sent back to the client
-  * @return true if the request was executed successfully, false otherwise
-    */
+    /**
+     * Refer documentation of {@link Replicable#execute(Request, boolean)} to
+     * understand what the boolean flag means.
+     * <p>
+     * You can assume that all requests will be of type {@link
+     * edu.umass.cs.gigapaxos.paxospackets.RequestPacket}.
+     *
+     * @param request
+     * @param b
+     * @return
+     */
     @Override
-    public boolean execute(Request request, boolean skipClientResponse) {
-    synchronized (this) {
-    try {
-    IntegerPacketType packetType = request.getRequestType();
-    String serviceName = request.getServiceName();
-    String reqValue = "";
+    public boolean execute(Request request, boolean b) {
+        // TODO: submit request to data store
+        // 'b' is skipClientResponse
+        boolean skipClientResponse = b; 
 
+        synchronized (this) {
+            try {
+                String reqValue = "";
 
-         // Only handle RequestPacket types
-         if (request instanceof RequestPacket) {
-             reqValue = ((RequestPacket) request).requestValue;
-         } else {
-             log.log(Level.WARNING, "Received request that is not a RequestPacket");
-         }
+                // Only handle RequestPacket types
+                if (request instanceof RequestPacket) {
+                    reqValue = ((RequestPacket) request).requestValue;
+                } else {
+                    log.log(Level.WARNING, "Received request that is not a RequestPacket");
+                }
 
-         // Ensure only allowed table is updated
-         String[] components = reqValue.split("\\s+");
-         if (components.length > 1 && "update".equals(components[0]) && !components[1].equals(table)) {
-             log.log(Level.SEVERE, "WARNING: Attempt to update table other than {0}", table);
-         }
+                // Ensure only allowed table is updated
+                String[] components = reqValue.split("\\s+");
+                if (components.length > 1 && "update".equals(components[0]) && !components[1].equals(table)) {
+                    log.log(Level.SEVERE, "WARNING: Attempt to update table other than {0}", table);
+                }
 
-         // Execute the query against Cassandra
-         ResultSet resultSet = session.execute(reqValue);
-         StringBuilder responseBuilder = new StringBuilder();
-         for (Row row : resultSet) {
-             responseBuilder.append(row.toString());
-         }
+                // Execute the query against Cassandra
+                ResultSet resultSet = session.execute(reqValue);
+                StringBuilder responseBuilder = new StringBuilder();
+                for (Row row : resultSet) {
+                    responseBuilder.append(row.toString());
+                }
 
-         if (!skipClientResponse && request instanceof RequestPacket) {
-             ((RequestPacket) request).setResponse(responseBuilder.toString());
-         }
+                if (!skipClientResponse && request instanceof RequestPacket) {
+                    ((RequestPacket) request).setResponse(responseBuilder.toString());
+                }
 
-         log.log(Level.INFO, "Executed request {0} with result {1}", new Object[]{reqValue, responseBuilder});
-         return true;
-     } catch (Exception e) {
-         log.log(Level.WARNING, "Exception during execute: {0}", e.toString());
-         return false;
-     }
-    
-
+                log.log(Level.INFO, "Executed request {0} with result {1}", new Object[]{reqValue, responseBuilder});
+                return true;
+            } catch (Exception e) {
+                log.log(Level.WARNING, "Exception during execute: {0}", e.toString());
+                return false;
+            }
+        }
     }
-    }
 
-  /**
-
-  * Default execute implementation that ignores client response.
-    */
+    /**
+     * Refer documentation of
+     * {@link edu.umass.cs.gigapaxos.interfaces.Application#execute(Request)}
+     *
+     * @param request
+     * @return
+     */
     @Override
     public boolean execute(Request request) {
-    log.log(Level.WARNING, "Default execute called; using skipClientResponse=true for request={0}", request);
-    return execute(request, true);
+        // TODO: execute the request by sending it to the data store
+        log.log(Level.WARNING, "Default execute called; using skipClientResponse=true for request={0}", request);
+        return execute(request, true);
     }
 
-  /**
-
-  * Creates a checkpoint of the current application state.
-  *
-  * @param checkpointName optional identifier for this checkpoint
-  * @return JSON string representing current state
-    */
+    /**
+     * Refer documentation of {@link Replicable#checkpoint(String)}.
+     *
+     * @param s
+     * @return
+     */
     @Override
-    public String checkpoint(String checkpointName) {
-    synchronized (this) {
-    log.log(Level.INFO, "Creating checkpoint with name={0}", checkpointName);
+    public String checkpoint(String s) {
+        // TODO:
+        String checkpointName = s;
+        synchronized (this) {
+            log.log(Level.INFO, "Creating checkpoint with name={0}", checkpointName);
 
-     String query = String.format("SELECT * FROM %s.%s", myID, table);
-     ResultSet rs = session.execute(query);
+            String query = String.format("SELECT * FROM %s.%s", myID, table);
+            ResultSet rs = session.execute(query);
 
-     Map<String, String> serializedRows = new HashMap<>();
+            Map<String, String> serializedRows = new HashMap<>();
 
-     for (Row row : rs) {
-         ColumnDefinitions cols = row.getColumnDefinitions();
-         int colIndex = 0;
-         String primaryKey = "UNINITIALIZED_KEY";
+            for (Row row : rs) {
+                ColumnDefinitions cols = row.getColumnDefinitions();
+                int colIndex = 0;
+                String primaryKey = "UNINITIALIZED_KEY";
 
-         for (ColumnDefinitions.Definition col : cols) {
-             String colName = col.getName();
-             String colValue = row.getObject(colName).toString();
+                for (ColumnDefinitions.Definition col : cols) {
+                    String colName = col.getName();
+                    String colValue = row.getObject(colName).toString();
 
-             if (colValue.isEmpty()) {
-                 colValue = "[]"; // Treat empty as empty list
-             }
+                    if (colValue.isEmpty()) {
+                        colValue = "[]"; // Treat empty as empty list
+                    }
 
-             String tuple = colName + "|" + colValue;
-             if (colIndex == 0) {
-                 primaryKey = tuple;
-                 serializedRows.put(primaryKey, "");
-             } else {
-                 serializedRows.put(primaryKey, serializedRows.get(primaryKey) + (colIndex == 1 ? tuple : "|" + tuple));
-             }
-             colIndex++;
-         }
-     }
+                    String tuple = colName + "|" + colValue;
+                    if (colIndex == 0) {
+                        primaryKey = tuple;
+                        serializedRows.put(primaryKey, "");
+                    } else {
+                        serializedRows.put(primaryKey, serializedRows.get(primaryKey) + (colIndex == 1 ? tuple : "|" + tuple));
+                    }
+                    colIndex++;
+                }
+            }
 
-     JSONObject json = new JSONObject(serializedRows);
-     log.log(Level.INFO, "Checkpoint data: {0}", json.toString());
-     return json.toString();
-    
-
+            JSONObject json = new JSONObject(serializedRows);
+            log.log(Level.INFO, "Checkpoint data: {0}", json.toString());
+            return json.toString();
+        }
     }
-    }
 
-  /**
-
-  * Restores the application state from a previously created checkpoint.
-  *
-  * @param checkpointName name of the checkpoint
-  * @param state JSON string representing the checkpoint
-  * @return true if restored successfully
-    */
+    /**
+     * Refer documentation of {@link Replicable#restore(String, String)}
+     *
+     * @param s
+     * @param s1
+     * @return
+     */
     @Override
-    public boolean restore(String checkpointName, String state) {
-    log.log(Level.INFO, "Restoring checkpoint {0} with state={1}", new Object[]{checkpointName, state});
+    public boolean restore(String s, String s1) {
+        // TODO:
+        String checkpointName = s;
+        String state = s1;
+        
+        log.log(Level.INFO, "Restoring checkpoint {0} with state={1}", new Object[]{checkpointName, state});
 
-    synchronized (this) {
-    try {
-    if (state == null || state.isEmpty()) {
-    return true; // Nothing to restore
+        synchronized (this) {
+            try {
+                if (state == null || state.isEmpty()) {
+                    return true; // Nothing to restore
+                }
+                JSONObject json = new JSONObject(state);
+                Map<String, String> stateMap = new HashMap<>();
+                Iterator<String> keys = json.keys();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    stateMap.put(key, json.getString(key));
+                }
+
+                for (String primaryKey : stateMap.keySet()) {
+                    String[] primaryParts = primaryKey.split("\\|");
+                    String[] colParts = stateMap.get(primaryKey).split("\\|");
+                    // Reconstruct the update query based on the serialized checkpoint format
+                    String updateQuery = "update " + table + " SET " + colParts[0] + "=" + colParts[1] + " where " + primaryParts[0] + "=" + primaryParts[1];
+                    session.execute(updateQuery);
+                    log.log(Level.INFO, "Restored row with query: {0}", updateQuery);
+                }
+
+                return true;
+            } catch (Exception e) {
+                log.log(Level.SEVERE, "Exception during restore: {0}", e);
+                return false;
+            }
+        }
     }
-         JSONObject json = new JSONObject(state);
-         Map<String, String> stateMap = new HashMap<>();
-         Iterator<String> keys = json.keys();
-         while (keys.hasNext()) {
-             String key = keys.next();
-             stateMap.put(key, json.getString(key));
-         }
 
-         for (String primaryKey : stateMap.keySet()) {
-             String[] primaryParts = primaryKey.split("\\|");
-             String[] colParts = stateMap.get(primaryKey).split("\\|");
-             String updateQuery = "update " + table + " SET " + colParts[0] + "=" + colParts[1] + " where " + primaryParts[0] + "=" + primaryParts[1];
-             session.execute(updateQuery);
-             log.log(Level.INFO, "Restored row with query: {0}", updateQuery);
-         }
 
-         return true;
-     } catch (Exception e) {
-         log.log(Level.SEVERE, "Exception during restore: {0}", e);
-         return false;
-     }
-
-    }
+    /**
+     * No request types other than {@link edu.umass.cs.gigapaxos.paxospackets
+     * .RequestPacket will be used by Grader, so you don't need to implement
+     * this method.}
+     *
+     * @param s
+     * @return
+     * @throws RequestParseException
+     */
+    @Override
+    public Request getRequest(String s) throws RequestParseException {
+        return null;
     }
 
-  @Override
-  public Request getRequest(String s) throws RequestParseException {
-  return null; // Not used for Grader
-  }
-
-  @Override
-  public Set<IntegerPacketType> getRequestTypes() {
-  return new HashSet<>(); // Only RequestPacket is expected
-  }
-  }
+    /**
+     * @return Return all integer packet types used by this application. For an
+     * example of how to define your own IntegerPacketType enum, refer {@link
+     * edu.umass.cs.reconfiguration.examples.AppRequest}. This method does not
+     * need to be implemented because the assignment Grader will only use
+     * {@link
+     * edu.umass.cs.gigapaxos.paxospackets.RequestPacket} packets.
+     */
+    @Override
+    public Set<IntegerPacketType> getRequestTypes() {
+        return new HashSet<IntegerPacketType>();
+    }
+}
